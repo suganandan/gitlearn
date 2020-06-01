9a0a6bdf54fda9d1bc91fabaf2bf14ba93af397f
root <root@ip-172-31-7-25.ap-south-1.compute.internal>
2020-06-01 16:18:00 +0000

    commit from feature1 branch

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("New line added");
+        System.out.println("New line added suganandan");
 	    System.out.println("Hello, World");
     }
 
